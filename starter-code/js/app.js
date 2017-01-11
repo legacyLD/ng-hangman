5b1a5b165353c085abedd295609868f476d52739
vedelopment <vedelopment@gmail.com>
2017-01-11 14:33:41 -0800

    complete step 3

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -2,4 +2,18 @@ console.log('app.js loaded!');
 
 // initialize the application
 angular
-  .module("hangmanApp", []);
+  .module("hangmanApp", [])
+  .controller("HangmanController", HangmanController);
+
+function HangmanController() {
+  var vm = this;
+  vm.hangman = new HangmanGame('elephant');
+  // vm.hangman.guess('e');
+  // vm.hangman.guess('f');
+  vm.guessLetter = function(input) {
+    // guess the letter
+    vm.hangman.guess(input);
+    // clear the input
+    vm.hangman.input = "";
+  }
+}
